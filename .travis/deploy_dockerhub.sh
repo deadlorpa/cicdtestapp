d1c4802ce2d65c70b60863a2d5710d459eb2bfac
deadlorpa <deadlorpa@gmail.com>
2021-05-29 00:35:06 +0700

    переписка деплоя

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -5,5 +5,5 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build tag alpine-img:latest $DOCKER_USER/alphine:latest .
-docker push $DOCKER_USER/alphine:latest
+docker build tag alpine-img:latest .
+docker push alphine:latest
