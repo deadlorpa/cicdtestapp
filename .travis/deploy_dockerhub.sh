81d5c43b8cc0759b3ec5462cc5b87cdc9c4235e1
deadlorpa <deadlorpa@gmail.com>
2021-05-29 11:11:40 +0700

    смотрю приколы

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -5,7 +5,5 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
-echo $TRAVIS_BRANCH
-echo $TRAVIS_REPO_SLUG
-docker build -t testapp:TAG $TRAVIS_REPO_SLUG .
+docker build -t testapp:TAG /home/travis/build/deadlorpa/CiCdTestApp .
 docker push testapp:TAG
