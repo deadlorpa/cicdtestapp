5720d164482747078de00d7918903e82eac426be
deadlorpa <deadlorpa@gmail.com>
2021-05-29 11:27:11 +0700

    смотрю приколы

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -t testapp:TAG /home/travis/build/deadlorpa/CiCdTestApp .
-docker push testapp:TAG
+docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
+docker push $TRAVIS_REPO_SLUG
