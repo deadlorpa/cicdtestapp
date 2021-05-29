0ffbf59c3068df5c3e06146e398f3af855e0f770
deadlorpa <deadlorpa@gmail.com>
2021-05-29 14:40:03 +0700

    heroku

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -2,4 +2,3 @@ wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 docker login --username=_ --password=$HEROKU_API_KEY registry.heroku.com
 heroku container:login
 heroku container:push web --app $HEROKU_APP_NAME
-heroku container:release web --app $HEROKU_APP_NAME
