4805d064764a32ea95e85d43245d5c481bd4ae78
deadlorpa <deadlorpa@gmail.com>
2021-05-29 11:47:22 +0700

    опять hub (тяжбы)

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -5,6 +5,6 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG
+docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
 docker tag $TRAVIS_REPO_SLUG:$TAG yanchkin/cicdtestapp:$TAG
 docker push yanchkin/cicdtestapp:$TA
