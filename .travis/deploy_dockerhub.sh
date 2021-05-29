3eb7b3a48b624dbc329f9b87945d5acbcfe9e031
deadlorpa <deadlorpa@gmail.com>
2021-05-29 11:41:28 +0700

    опять hub :)

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker login -u $DOCKER_USER -p $DOCKER_PASS
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG yanchkin/cicdtestapp:$TAG .
+docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG
+docker tag $TRAVIS_REPO_SLUG:$TAG yanchkin/cicdtestapp:$TAG
 docker push yanchkin/cicdtestapp:$TA
